ea5e654bd5fad54aa923deea9421ef76125353dc
Kahlil J Richards <46690410+Kahlil-Richards@users.noreply.github.com>
2023-11-29 06:58:51 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,14 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className='section'>
+<div className="home__welcome">
+
+//SVG with className home__icon
+
+<h1 className="title">Let's bring them home</h1>
+<h1 className="description">text</h1>
+<a className="button 
+</a>
       <h1 className="title">Home</h1>
       <Link href="/about"></Link>
     </main>
