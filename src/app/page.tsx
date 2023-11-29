30c605527f946bf10de41c02bfed1950bbaaa4a1
Kahlil-Richards <kahlil.j.richards@gmail.com>
2023-11-29 13:46:10 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import live-thumbnail from '../../public/img'
+import live_thumbnail from '../../public/img'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from './page.module.css'
@@ -6,32 +6,34 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className='section'>
-<div className="home__welcome">
+      <div className="home__welcome">
 
 //SVG with className home__icon
 
-<h1 className="title">Let's bring them home</h1>
-<h1 className="description">text</h1>
-<a className="button data-name="color" data-value="blue">
-<h1 className="button__text">
-</a>
-</div>
+        <h1 className="title">Let's bring them home</h1>
+        <h1 className="description">text</h1>
+        <a className="button" data-name="color" data-value="blue">
+          <h1 className="button__text"></h1>
+        </a>
+      </div>
 
-<div className="home__events">
-<div className="home__event-wrapper>
-JavaScript to check database and return info as tag (like react components)
-</div>
-</div>
+      <div className="home__events">
+        <div className="home__event-wrapper">
+// JavaScript to check database and return info as tag (like react components)
+        </div>
+      </div>
 
-<div className=home__main">
-<div className="home__main-group">
-<Image className="home__main-img" scr={live-thumbnail}/>
-<div className="home__main-info">
+      <div className="home__main">
+        <div className="home__main-group">
+          <Image className="home__main-img" src={live_thumbnail} />
+          <div className="home__main-info">
 
-</div>
+          </div>
 
-      <h1 className="title">Home</h1>
-      <Link href="/about"></Link>
+          <h1 className="title">Home</h1>
+          <Link href="https://www.youtube.com/@meadowbrookunitedchurch82/live" target='_blank'></Link>
+        </div>
+      </div>
     </main>
   )
 }
